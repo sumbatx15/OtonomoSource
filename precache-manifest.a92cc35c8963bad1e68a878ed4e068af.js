56a384f702abc5a7dfc1145c9b246d6bdbb89cf5
sumbat15 <sumbat15@gmail.com>
2019-07-14 17:21:30 +0300

    Updates

---
+++ precache-manifest.a92cc35c8963bad1e68a878ed4e068af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a08a2777e30244cc32cf7be8a023340c",
+    "url": "/OtonomoSource/static/media/map.a08a2777.png"
+  },
+  {
+    "revision": "e141bc5cc095cd86eaa12712c51fe93b",
+    "url": "/OtonomoSource/static/media/loading.e141bc5c.gif"
+  },
+  {
+    "revision": "dd6aafe8a968153c4162",
+    "url": "/OtonomoSource/static/js/runtime~main.dd6aafe8.js"
+  },
+  {
+    "revision": "9912cb854d904286dea1",
+    "url": "/OtonomoSource/static/js/main.9912cb85.chunk.js"
+  },
+  {
+    "revision": "2eef86b85433dc68b0cc",
+    "url": "/OtonomoSource/static/js/1.2eef86b8.chunk.js"
+  },
+  {
+    "revision": "9912cb854d904286dea1",
+    "url": "/OtonomoSource/static/css/main.278c876c.chunk.css"
+  },
+  {
+    "revision": "2eef86b85433dc68b0cc",
+    "url": "/OtonomoSource/static/css/1.7b9ce5b4.chunk.css"
+  },
+  {
+    "revision": "9730235ab5d5150cacb3f302840aec4b",
+    "url": "/OtonomoSource/index.html"
+  }
+];
